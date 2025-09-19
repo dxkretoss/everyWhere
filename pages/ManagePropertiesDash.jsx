91a4f180f801a87903f70db2640c92df003fc899
dxkretoss <dakshn@kretoss.in>
2025-09-19 13:09:55 +0530

    add ManagePropertiesDash page

---
+++ pages/ManagePropertiesDash.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AccMainlayout from '@/components/AccMainlayout/AccMainlayout'
+
+export default function ManagePropertiesDash() {
+    return (
+        <AccMainlayout>ManagePropertiesDash</AccMainlayout>
+    )
+}
